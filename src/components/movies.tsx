5de9de186ab974e38fe0129ca378153b510a7a6d
Ivo Angelov <jc@jagdcake.com>
2020-03-04 15:48:39 +0200

    Split movie props into three
    Needed to define props for info card and misc metadata components which
    will then be used to create a movie component.

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -3,14 +3,20 @@ import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
 import { LinkUrl } from './header';
 
-interface Movie {
-    id: number;
+interface InfoCardProps {
     title: string;
     yearOfRelease: number;
     directors: [string];
     topActors: [string];
+}
+
+interface MiscMetadataProps {
     myRating: string;
     watchedOn: string;
     discussion: LinkUrl;
     imdbId: string;
+}
+
+interface MovieProps extends InfoCardProps, MiscMetadataProps {
+    id: number;
 }
