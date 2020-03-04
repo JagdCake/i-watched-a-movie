36ae65d5ffe71b995142823688888789be68fa6b
Ivo Angelov <jc@jagdcake.com>
2020-03-04 16:14:14 +0200

    Create movie info card component
    Displays basic movie data: title, release year, director(s), and the
    first two top actors (according to IMDb).

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -10,6 +10,57 @@ interface InfoCardProps {
     topActors: [string, string];
 }
 
+const InfoCard: FunctionComponent<InfoCardProps> = ({
+    title,
+    yearOfRelease,
+    directors,
+    topActors,
+}: InfoCardProps): ReactElement => {
+    const listOfDirectors: ReactElement[] = directors.map((director, index) => {
+        return <li key={index}>{director}</li>;
+    });
+
+    return (
+        <>
+            <div
+                aria-label="Movie info card top part"
+                className="border-b-2 border-black h-56 px-5 flex flex-wrap content-center"
+            >
+                <h1
+                    aria-label="title"
+                    className="text-2xl font-bold mb-2 w-full"
+                >
+                    {title}
+                </h1>
+                <h2 aria-label="release year" className="text-lg w-full">
+                    {yearOfRelease}
+                </h2>
+            </div>
+
+            <div
+                aria-label="Movie info card bottom part"
+                className="h-56 px-5 flex flex-wrap content-center"
+            >
+                <h1 className="text-sm w-full">Directed by</h1>
+                <ul
+                    aria-label="director(s)"
+                    className="text-xl font-bold mb-2 w-full"
+                >
+                    {listOfDirectors}
+                </ul>
+                <h2 className="text-sm w-full">Starring</h2>
+                <ul
+                    aria-label="first two top actors"
+                    className="text-xl font-bold w-full"
+                >
+                    <li>{topActors[0]}</li>
+                    <li>{topActors[1]}</li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
 interface MiscMetadataProps {
     myRating: string;
     watchedOn: string;
