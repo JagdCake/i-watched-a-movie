156d4923c3f59be4a2018f5ba3d831eb5e3d2687
Ivo Angelov <jc@jagdcake.com>
2020-03-16 13:35:54 +0200

    Create page navigation component
    Displays links to the first and last pages, the current page number and
    links to the previous and next pages.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
+import { Link } from 'gatsby';
 
 export interface PageNavProp {
     pageContext: {
@@ -10,3 +11,42 @@ export interface PageNavProp {
         currentPage: number;
     };
 }
+
+const PageNav: FunctionComponent<PageNavProp> = ({
+    pageContext,
+}: PageNavProp): ReactElement => {
+    const currentPage: number = pageContext.currentPage;
+    const totalPages: number = pageContext.numPages;
+
+    return (
+        <nav>
+            <ol>
+                <li className="inline m-1 p-1 bg-black text-red-500 rounded-sm">
+                    <Link className="p-1" to="/page/1`">
+                        1
+                    </Link>
+                </li>
+
+                <li className="inline m-1 p-1">
+                    <Link className="p-1" to={`/page/${currentPage - 1}`}>
+                        ❮
+                    </Link>
+                </li>
+
+                <li className="inline m-1 p-1">{currentPage}</li>
+
+                <li className="inline m-1 p-1">
+                    <Link className="p-1" to={`/page/${currentPage + 1}`}>
+                        ❯
+                    </Link>
+                </li>
+
+                <li className="inline m-1 p-1 bg-black text-red-500 rounded-sm">
+                    <Link className="p-1" to={`/page/${totalPages}`}>
+                        {totalPages}
+                    </Link>
+                </li>
+            </ol>
+        </nav>
+    );
+};
