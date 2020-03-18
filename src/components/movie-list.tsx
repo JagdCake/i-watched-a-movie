deaf3829df36f3daba6a00317a2126005178fa28
Ivo Angelov <jc@jagdcake.com>
2020-03-18 15:45:26 +0200

    Define props for movie link list component

--- src/components/movie-list.tsx
+++ src/components/movie-list.tsx
@@ -7,3 +7,8 @@ export interface MovieSearchData {
     title: string;
     yearOfRelease: number;
 }
+
+interface MovieLinkListProps {
+    displayStatus: boolean;
+    movieData: MovieSearchData[];
+}
