02a3c2d4c60d6091f6754974480e1ab5f263fc84
Ivo Angelov <jc@jagdcake.com>
2020-03-18 15:33:02 +0200

    Create type for a movie search data query

---
+++ src/components/movie-list.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
+
+export interface MovieSearchData {
+    id: number;
+    title: string;
+    yearOfRelease: number;
+}
