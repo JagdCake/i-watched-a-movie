bfd7187bf5ab65b134796a764ac1b29d7b4dca6b
Ivo Angelov <jc@jagdcake.com>
2020-03-26 16:28:45 +0200

    Create "back to top" link component
    Displays a link represented by an up arrow icon with lower opacity.

--- src/components/back-to-top-link.tsx
+++ src/components/back-to-top-link.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
+import { Link } from 'gatsby';
 import upIcon from '../images/up.svg';
 
 interface BackToTopLinkProp {
     linkToTopOfWindow: string;
 }
+
+const BackToTopLink: FunctionComponent<BackToTopLinkProp> = ({
+    linkToTopOfWindow,
+}: BackToTopLinkProp): ReactElement => {
+    return (
+        <Link
+            title="Back to top"
+            to={linkToTopOfWindow}
+            className="opacity-75 hover:opacity-100 focus:opacity-100"
+        >
+            <span className="absolute right-0 m-3 p-2 bg-black rounded-full shadow-black z-10">
+                <img className="w-full" src={upIcon} alt="Up arrow icon"></img>
+            </span>
+        </Link>
+    );
+};
+
+export default BackToTopLink;
