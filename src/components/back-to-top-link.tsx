a8f7970cebe45ce77243d9e7f877f2a619652a99
Ivo Angelov <jc@jagdcake.com>
2020-03-26 16:28:09 +0200

    Add an up arrow icon

--- src/components/back-to-top-link.tsx
+++ src/components/back-to-top-link.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FunctionComponent } from 'react';
 import { ReactElement } from 'react';
+import upIcon from '../images/up.svg';
 
 interface BackToTopLinkProp {
     linkToTopOfWindow: string;
