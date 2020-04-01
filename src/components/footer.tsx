c92751f5b4caafa7a46287ca91f50caf1573405a
Ivo Angelov <jc@jagdcake.com>
2020-04-01 10:14:03 +0300

    Match the width of the footer with the header width, on small phones

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -111,7 +111,7 @@ const Footer: FunctionComponent<PageNavProp> = ({
     pageContext,
 }: PageNavProp): ReactElement => {
     return (
-        <footer className="w-full bg-red text-center text-2xl p-4 lg:w-2/5 lg:m-auto">
+        <footer className="w-full bg-red text-center text-2xl p-4 sm:w-10/12 m-auto lg:w-2/5">
             <PageNav pageContext={pageContext} />
         </footer>
     );
