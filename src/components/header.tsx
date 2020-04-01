87db4780bf682cea3909e5b1c063a0ee9c868fa8
Ivo Angelov <jc@jagdcake.com>
2020-04-01 14:31:46 +0300

    Increase font size of site title heading, on desktops

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,7 @@ interface TitleHeadingProp {
 const TitleHeading: FunctionComponent<TitleHeadingProp> = ({
     title,
 }): ReactElement => (
-    <h1 className="text-center text-2xl font-medium tracking-wide font-slackey bg-red py-3 sm:rounded-b-sm">
+    <h1 className="text-center text-2xl font-medium tracking-wide font-slackey bg-red py-3 sm:rounded-b-sm 3xl:text-3xl">
         <Link
             className="hover:font-black focus:font-black active:font-black"
             to="/"
