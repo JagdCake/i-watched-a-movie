98db9e8f3c06670f42085e9fcb8e38769ddd67b5
Ivo Angelov <jc@jagdcake.com>
2020-04-28 17:18:52 +0300

    Set realistic page context values
    Also, make the mock header props available to all describe blocks in the
    test file.

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Header from '../header';
 
-describe(`Header's About section`, () => {
-    const mockHeaderProps = {
-        siteTitle: 'Test Site',
-        searchData: [
-            {
-                id: 1,
-                title: 'Test Movie',
-                yearOfRelease: 2020,
-            },
-        ],
-        pageContext: {
-            first: 0,
-            offset: 0,
-            numPages: 0,
-            currentPage: 0,
-            numberOfMovies: 0,
+const mockHeaderProps = {
+    siteTitle: 'Test Site',
+    searchData: [
+        {
+            id: 1,
+            title: 'Test Movie',
+            yearOfRelease: 2020,
         },
-    };
+    ],
+    pageContext: {
+        first: 1,
+        offset: 0,
+        numPages: 1,
+        currentPage: 1,
+        numberOfMovies: 1,
+    },
+};
 
+
+describe(`Header's About section`, () => {
     it(`should be hidden by default`, () => {
         const { getByLabelText } = render(<Header {...mockHeaderProps} />);
 
