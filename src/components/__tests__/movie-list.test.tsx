3eb7964c6c46ca44b620ec0db1b80cdfb3e7c5ef
Ivo Angelov <jc@jagdcake.com>
2020-05-03 10:33:42 +0300

    Test if the generated page numbers for the movie links are correct

--- src/components/__tests__/movie-list.test.tsx
+++ src/components/__tests__/movie-list.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import MovieLinkList from '../movie-list';
+import { MovieLinkListProps } from '../movie-list';
 
 const mockMovieLinkListProps = (movieData: Array<{}> = [], pageContext: {}) => {
     return {
@@ -40,4 +42,36 @@ describe(`if there are 2 movies and you want 1 per page`, () => {
         currentPage: currentPage,
         numberOfMovies: numberOfMovies,
     };
+
+    const props = mockMovieLinkListProps(aSecondMovie, pageContextForTwoMovies);
+
+    it(`the link to the first movie should lead to page 2`, () => {
+        const { getByLabelText } = render(
+            <MovieLinkList {...(props as MovieLinkListProps)} />
+        );
+
+        const movieList = getByLabelText(/list.*movie.*titles/i);
+        const movieLinks = Array.from(movieList.querySelectorAll('a')) as Array<
+            HTMLAnchorElement
+        >;
+
+        // the first movie is second in the array because the movies are
+        // ordered by their IDs, in descending order
+        const linkToFirstMovie = movieLinks[1];
+        expect(linkToFirstMovie.href).toMatch(/page\/2#1/);
+    });
+
+    it(`the link to the second movie should lead to page 1`, () => {
+        const { getByLabelText } = render(
+            <MovieLinkList {...(props as MovieLinkListProps)} />
+        );
+
+        const movieList = getByLabelText(/list.*movie.*titles/i);
+        const movieLinks = Array.from(movieList.querySelectorAll('a')) as Array<
+            HTMLAnchorElement
+        >;
+
+        const linkToSecondMovie = movieLinks[0];
+        expect(linkToSecondMovie.href).toMatch(/page\/1#2/);
+    });
 });
